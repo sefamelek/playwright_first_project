a3a91ee73570fc9548f065f6a54b36565a6fdaa1
Sefa Melek <sefamelek868@hotmail.com>
2023-05-23 15:09:01 +0300

    update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
+  await page.goto('https://www.twitter.com/');
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Twitter/);
+  await page.locator()
+  await page.getByTestId('login').click;
+
+
+
 });
 
 test('get started link', async ({ page }) => {
